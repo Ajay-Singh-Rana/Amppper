39b4b126df789a2d481b2bee6e270b6ab21bab71
Ajay Singh Rana <ajaysinghrana494@gmail.com>
2021-09-07 14:30:48 +0530

    Implement join audio graphical interface Implement the GUI for joining two audio files also add features to queue tasks

--- aud_process.py
+++ aud_process.py
@@ -10,85 +10,135 @@ import tkinter as tk
 import os
 from tkinter import filedialog
 from tkinter import ttk
-
-class Logo(tk.Frame):
-    def __init__(self,parent,controller):
-        tk.Frame.__init__(self,parent)
-        self.controller = controller
-
-        self.background = tk.PhotoImage(file='icons/amppper.png')
-        canvas = tk.Canvas(self,height = self.background.height(),width = self.background.width())
-        canvas.pack(fill='both',expand = True)
-        canvas.create_image((0,0),image = self.background,anchor = 'nw')
+from tkinter import messagebox
+import threading
 
 class ChangeFormat(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
         self.controller = controller
+        self.config(bg = '#2AA2BC')
 
-        formats = ttk.Combobox(self,values = ('.mp3','.flv','.ogg','.wav','.wma','.avi'))
-        formats.pack()
+        formats = ttk.Combobox(self,state = 'readonly',values = ('.mp3','.flv','.ogg','.wav','.wma','.avi','.aac','.m4a','flac','opus','au','aiff','webm'))
+        formats.pack(padx = 10,pady = 20)
 
+        formats.current(0)
 
 class JoinAudio(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
         self.controller = controller
-        filename_1 ,filename_2 = '',''
+        self.filename_1 ,self.filename_2 = None,None
+        self.config(bg = '#2AA2BC')
+        self.thread_count = 0
 
-        file_label_1 = tk.Label(self,text = '---Empty Selection---',width = 15)
-        file_label_1.grid(row = 0,column = 0,padx= 10,pady = 10)
+        self.file_label_1 = tk.Label(self,text = '---Empty Selection---',width = 20)
+        self.file_label_1.grid(row = 0,column = 0,padx= 10,pady = 10)
 
-        add_1 = tk.Button(self,text = 'Select File',command = lambda:browse_file(filename_1))
+        add_1 = tk.Button(self,text = 'Select File',command = lambda:self.browse_file(1),relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
         add_1.grid(row = 0,column = 1,padx = 10,pady = 10)
 
-        file_label_2 = tk.Label(self,text = '---Empty Selection---',width = 15)
-        file_label_2.grid(row = 1,column = 0,padx = 10,pady =10)
+        self.file_label_2 = tk.Label(self,text = '---Empty Selection---',width = 20)
+        self.file_label_2.grid(row = 1,column = 0,padx = 10,pady =10)
 
-        add_2 = tk.Button(self,text='Select File')
+        add_2 = tk.Button(self,text='Select File',relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d',command = lambda:self.browse_file(2))
         add_2.grid(row = 1,column = 1,padx = 10,pady =10)
 
-        export_as = ttk.Combobox(self,values = ('.mp3','.flv','.ogg','.wav','.wma','.avi'))
-        export_as.grid(row = 2, column = 0,padx = 10,pady = 10)
+        self.export_as = ttk.Combobox(self,values = ('mp3','flv','ogg','wav','wma','avi','aac','m4a','flac','opus','au','aiff','webm'),state = 'readonly')
+        self.export_as.grid(row = 2, column = 0,padx = 10,pady = 10)
 
-        export_as.current(0)
+        self.export_as.current(0)
+
+        join_ = tk.Button(self,text='Join',relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d',command = self.run_thread)
+        join_.grid(row = 2,column = 1,padx = 10,pady = 10)
+
+        self.label = tk.Label(self,text = 'No processs...',width = 20,bg = '#2a8d12',fg = 'White',relief = 'sunken')
+        self.label.grid(row = 3,column = 0)
+
+        self.queue = tk.Label(self,text = 'Queued : 0',width = 10,bg = '#2a8d12',fg = 'White',relief = 'sunken')
+        self.queue.grid(row = 3,column = 1,padx = 10,pady = 10)
     
-    def browse_file(filename):
+    def browse_file(self,num):
         filename = filedialog.askopenfilename()
-        if(filename):
-            file_label_1.config(text = filename)
+        if(num == 1):
+            self.filename_1 = filename
+        else:
+            self.filename_2 = filename
+        self.set_labels()
+    
+    def set_labels(self):
+        if(self.filename_1):
+            self.file_label_1.config(text = os.path.split(self.filename_1)[1])
+        
+        if(self.filename_2):
+            self.file_label_2.config(text = os.path.split(self.filename_2)[1])
 
+    def run_thread(self):
+        self.thread_count += 1
+        thread = threading.Thread(target = self.join_audio)
+        thread.start()
 
+    def join_audio(self):
+        if(self.filename_1 and self.filename_2):
+            ext = self.export_as.get()
+            format_ = {'mp3':'mp3','flv':'flv','ogg':'ogg','wav':'wav','wma':'wma','avi':'avi','aac':'adts','m4a':'mp4','flac':'flac','opus':'opus','au':'au','aiff':'aiff','webm':'webm'}
+            filename = filedialog.asksaveasfilename(defaultextension = f'.{ext}')
+            if(filename):
+                try:
+                    self.label.config(text = 'Processing...',bg = '#2a8d12')
+                    self.queue.config(text = f'Queued : {self.thread_count}')
+                    audio_1 = AudioSegment.from_file(self.filename_1,format_[os.path.splitext(self.filename_1)[1][1:]])
+                    self.filename_1 = ''
+                    self.file_label_1.config(text = '---Empty Selection---')
+                    audio_2 = AudioSegment.from_file(self.filename_2,format_[os.path.splitext(self.filename_2)[1][1:]])
+                    self.filename_2 = ''
+                    self.file_label_2.config(text = '---Empty Selection---')
+                    final = audio_1 + audio_2
+                    final.export(filename,format = format_[ext])
+                    self.label.config(text = 'Success..!',bg = '#2a8d12')
+                except:
+                    self.label.config(text = 'Failed..!',bg = '#ee3456')
 
-
-def join_audio(filename_1,filename_2,export_as,ext):
-    audio_1 = AudioSegment.from_file(file_name_1,os.path.splitext(filename_1)[1][1:])
-    audio_2 = AudioSegment.from_file(file_name_2,os.path.splitext(filename_2)[1][1:])
-    final = audio_1 + audio_2
-    final.export(f'{export_as}.{ext}',format = ext)
+        else:
+            messagebox.showerror(title = 'Error..!',message = 'Select the files first..!')
+        
+        self.thread_count -= 1
+        self.queue.config(text = f'Queued : {self.thread_count}')
 
 
 def change_format(file_name,to):
     pass
 
-class AudioEditor(tk.Toplevel):
-    def __init__(self):
-        super().__init__()
-        self.geometry('610x450')
-        self.title('Audio Tools')
-        self.resizable(False,False)
+class AudioEditor(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        self.controller = controller
+        #self.geometry('610x450')
+        #self.title('Amppper Audio Tools')
+        #self.resizable(False,False)
         self.frames = {}
+        self.background = tk.PhotoImage(file = 'icons/amppper.png')
+        im_height,im_width = self.background.height(), self.background.width()
 
-        container = tk.Frame(self)
-        container.pack(expand = True,side = 'bottom',fill = 'both')
+        background = tk.Canvas(self,height = im_height,width = im_width)
+        background.pack(fill = 'both',expand = True)
 
-        button = tk.Button(self,text = 'Change Format',width = 15,command = lambda:self.show_frame('ChangeFormat'))
-        button.pack(padx=10)
+        background.create_image(0,0,image = self.background,anchor = 'nw')
 
-        audio_join = tk.Button(self,text = 'Join Audio',width = 15,command = lambda:self.show_frame('JoinAudio'))
-        audio_join.pack(padx=10)
+        container = tk.Frame(self,width = 300,height = 250)
+        container.place(x = 155,y = 160)
+
+        button = tk.Button(background,text = 'Convert',width = 5,command = lambda:self.show_frame('ChangeFormat'),relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
+        button.place(x = 30,y = 30)
+
+        audio_join = tk.Button(background,text = 'Join',width = 5,command = lambda:self.show_frame('JoinAudio'),relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
+        audio_join.place(x = 130,y = 30)
         
-        for f in (ChangeFormat,JoinAudio,Logo):
+        button = tk.Button(background,text = 'Home',width = 5,command = lambda:self.controller.show_frame('MainFrame'),relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
+        button.place(x = 230,y = 30)
+
+
+        for f in (ChangeFormat,JoinAudio):
             page_name = f.__name__
             frame = f(parent = container,controller = self)
             self.frames[page_name] = frame
--- main.py
+++ main.py
@@ -9,24 +9,19 @@ import tkinter as tk    # importing the tkinter library as this is what we will 
 from tkinter import filedialog
 from img_process import ImageEditor
 from aud_process import AudioEditor
+import os
 
-# main window 
-class App(tk.Tk):
-    """
-    This class defines the main window of the GUI and all it's content widgets.
-    """
+if(not os.path.isdir('Amppper')):
+    os.mkdir('Amppper')
 
-    def __init__(self) -> None:
-        super().__init__()
-        # x = int((self.winfo_screenwidth() - 800)/2)
-        # y = int((self.winfo_screenheight() - 600)/2)
-        # self.geometry(f'800x600+{x}+{y}')
-        self.title('Amppper')
+class MainFrame(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        self.controller = controller
+
         self.background = tk.PhotoImage(file = 'icons/amppper.png')
         im_height,im_width = self.background.height(), self.background.width()
-        self.geometry(f'{im_width}x{im_height}')
-        self.resizable(False,False)
-        
+
         background = tk.Canvas(self,height = im_height,width = im_width)
         background.pack(fill = 'both',expand = True)
 
@@ -35,12 +30,12 @@ class App(tk.Tk):
         img_button = tk.Button(background,text = 'Image Options',pady = 5,padx = 5,width = 15,command = self.image_options,relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
         img_button.place(x = 420, y = 170)
 
-        audio_button = tk.Button(background,text = 'Audio Options',pady = 5,padx = 5,width = 15,command = AudioEditor,relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
+        audio_button = tk.Button(background,text = 'Audio Options',pady = 5,padx = 5,width = 15,command = lambda:self.controller.show_frame('AudioEditor'),relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
         audio_button.place(x = 420, y = 210)
 
         video_button = tk.Button(background,text = 'Video Options',pady = 5,padx = 5,width = 15,command = self.video_options,relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d')
         video_button.place(x = 420, y = 250)
-        
+
     def image_options(self):
         popup = tk.Toplevel()
         
@@ -60,6 +55,36 @@ class App(tk.Tk):
  
     def video_options(self):
         pass
+
+# main window 
+class App(tk.Tk):
+    """
+    This class defines the main window of the GUI and all it's content widgets.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        # x = int((self.winfo_screenwidth() - 800)/2)
+        # y = int((self.winfo_screenheight() - 600)/2)
+        # self.geometry(f'800x600+{x}+{y}')
+        self.title('Amppper')
+        self.geometry('610x390')
+        self.resizable(False,False)
+        self.frames = {}
+        #self.wm_attributes('-transparentcolor','#a1a1a1')
+
+        background = tk.Frame(self)
+        background.pack(fill = 'both',expand = True)
+
+        for f in (AudioEditor,MainFrame):
+            page_name = f.__name__
+            frame = f(parent = background,controller = self)
+            self.frames[page_name] = frame
+            frame.grid(row = 0,column = 0,sticky = 'nesw')
+        
+    def show_frame(self,name):
+        frame = self.frames[name]
+        frame.tkraise()
 
 if __name__ == "__main__":
     app = App()
