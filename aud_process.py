ab33f71fb8e5cd37dd073be395d6cf7bc1249c0a
Ajay Singh Rana <ajaysinghrana494@gmail.com>
2021-09-10 20:54:32 +0530

    Implement Audio Trim functionality
    Add a button to trim audio files

--- aud_process.py
+++ aud_process.py
@@ -36,37 +36,114 @@ class Trim(tk.Frame,Browse):
 
         self.label = tk.Label(self,text = '---Empty Selection---',
         width = 20,pady = 5,bg = '#ee3456')
-        self.label.grid(row = 0,column = 0,padx = 10,pady = 10)
+        self.label.grid(row = 0,column = 0,padx = 10,pady = 11)
 
         self.add = tk.Button(self,text = 'Select File',bg = '#2abc8d',
         relief = 'flat',activebackground = '#aabc8d',width = 10,
         command = self.browse)
-        self.add.grid(row = 0,column = 1,padx = 10,pady = 10)
+        self.add.grid(row = 0,column = 1,padx = 10,pady = 12)
 
         self.display_length = tk.Label(self,text = 'Duration: 00:00',
-        bg = '#aa89ca',relief = 'sunken')
-        self.display_length.grid(row = 1,column = 0,columnspan = 2)
+        bg = '#aa89ca',relief = 'sunken',pady = 5,padx = 5)
+        self.display_length.grid(row = 1,column = 0,columnspan = 2,padx = 10,pady = 5)
+
+        self.from_label = tk.Label(self,bg = '#2AA2BC')
+        self.from_label.grid(row = 2,column = 0,padx = 10,pady = 5)
+
+        self.from_ = tk.Label(self.from_label,bg = '#2AA2BC',text = 'From:')
+        self.from_.grid(row = 0,column = 0)
+
+        self.from_spin = tk.Spinbox(self.from_label,relief = 'sunken',
+        selectbackground = '#aa348c',state = 'disable',width = 6)
+        self.from_spin.grid(row = 0,column = 1)
 
         self.to_label = tk.Label(self,bg = '#2AA2BC')
-        self.to_label.grid(row = 2,column = 0,padx = 10,pady = 10)
+        self.to_label.grid(row = 2,column = 1,padx = 10,pady = 5)
 
-        self.to = tk.Label(self.to_label,bg = '#2AA2BC',text = 'From:')
+        self.to = tk.Label(self.to_label,bg = '#2AA2BC',text = 'To:')
         self.to.grid(row = 0,column = 0)
 
         self.to_spin = tk.Spinbox(self.to_label,relief = 'sunken',
-        selectbackground = '#aa348c',state = 'disable')
+        selectbackground = '#aa348c',state = 'disable',width = 6)
         self.to_spin.grid(row = 0,column = 1)
 
+        trim = tk.Button(self,text = 'Trim',width = 10,
+        relief = 'flat',bg = '#2abc8d',activebackground = '#aabc8d',
+        command = self.run_thread)
+        trim.grid(row = 4,column = 0,columnspan = 2,padx = 10,pady = 5)
 
+        self.status = tk.Label(self,text = 'No process..!',
+        width = 20,pady = 5,bg = '#ae34d9',fg = 'White',relief = 'sunken')
+        self.status.grid(row = 5,column = 0,padx = 10,pady = 5)
+
+        self.queue = tk.Label(self,text = 'Queued : 0',
+        width = 10,pady = 5,bg = '#ae34d9',fg = 'White',relief = 'sunken')
+        self.queue.grid(row = 5,column = 1,padx = 10,pady = 5)
+
+    
+    def run_thread(self):
+        thread = threading.Thread(target = self.trim)
+        self.thread += 1
+        thread.start()
+    
     def browse(self):
         Browse.browse(self)
-        try:
-            audio = AudioSegment.from_file(self.filename)
-            duration = audio.duration_seconds
-            self.display_length.config(text = 'Duration : {:.2f} secs'.format(duration))
-        except:
+        self.label.update()
+        if(self.filename):
+            try:
+                self.audio = AudioSegment.from_file(self.filename)
+                duration = self.audio.duration_seconds
+                self.display_length.config(text = 'Duration : {:.2f} secs'.format(duration))
+                self.from_spin.config(state = 'normal',to = duration,from_ = 0,)
+                self.to_spin.config(state = 'normal',to = duration,from_ = 0)
+            except:
+                self.label.config(text = '---Empty Selection---')
+                messagebox.showerror(title = 'Error',message = 'Incompatible file format..!')
+
+    def trim(self):
+        if(self.filename):
+            ext = os.path.splitext(self.filename)[1]
+            filename = filedialog.asksaveasfilename(defaultextension = ext,filetypes = ((f'{ext} files',f'*{ext}'),))
+            if(filename):
+                try:
+                    format_ = {'mp3':'mp3','flv':'flv','ogg':'ogg','wav':'wav','wma':'wma',
+                            'avi':'avi','aac':'adts','m4a':'mp4','flac':'flac','opus':'opus','au':'au',
+                            'aiff':'aiff','webm':'webm'}
+                    try:
+                        start = int(float(self.from_spin.get()) * 1000)
+                        end = int(float(self.to_spin.get()) * 1000)
+                        if(start > end):
+                            raise
+                        
+                        self.status.config(text = 'Processing...',bg = '#2a8d12')
+                        self.queue.config(text = f'Queued: {self.thread}',bg = '#2a8d12')
+                        self.label.config(text = '---Empty Selection---')
+                        self.to_spin.delete(0,'end')
+                        self.from_spin.delete(0,'end')
+                        self.filename = ''
+                        to_be_saved = self.audio[start : end]
+                        to_be_saved.export(filename,format = format_[ext[1:]])
+                        self.status.config(text = 'Success..!',bg = '#2a8d12')
+                    except:
+                        messagebox.showerror(title = 'Error...',
+                        message = 'Enter the correct values for the To and From..!')
+                except:
+                    self.status.config(text = 'Failed..!',bg = '#ee3456')
+                    messagebox.showerror(title = 'Error...',message = 'Incompatible file..!')
             self.label.config(text = '---Empty Selection---')
-            messagebox.showerror(title = 'Error',message = 'Incompatible file format..!')
+            self.display_length.config(text = 'Duration: 00:00')
+            self.to_spin.delete(0,'end')
+            self.from_spin.delete(0,'end')
+        else:
+                messagebox.showerror(title = 'Error...',message = 'Select a file to Trim..!')
+        
+        self.thread -= 1
+        self.queue.config(text = f'Queued: {self.thread}',bg = '#2a8d12')
+        if(self.thread == 0):
+            time.sleep(1)
+            self.queue.config(bg = '#ae34d9')
+            self.status.config(text = 'No process..!',bg = '#ae34d9')
+
 
 # frame for changing audio formats
 class ChangeFormat(tk.Frame,Browse):
